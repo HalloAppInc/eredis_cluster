d98eae46b031a4da449c49e99d54ba05f3dd638b
Tiroshan <tirosham@gmail.com>
2017-08-14 09:51:11 +0530

    initialization of pool_size & max_pool_flow_size are defined at the pool creation.

--- src/eredis_cluster_pool.erl
+++ src/eredis_cluster_pool.erl
@@ -7,7 +7,7 @@
 -export([transaction/2]).
 
 %% Supervisor
--export([start_link/0]).
+-export([start_link/0, start_link/1]).
 -export([init/1]).
 
 -include("eredis_cluster.hrl").
@@ -21,8 +21,14 @@ create(Host, Port) ->
         undefined ->
             WorkerArgs = [{host, Host}, {port, Port}],
 
-        	Size = application:get_env(eredis_cluster, pool_size, 10),
-        	MaxOverflow = application:get_env(eredis_cluster, pool_max_overflow, 0),
+            case ets:info(?MODULE) of
+                undefined ->
+                    Size = application:get_env(eredis_cluster, pool_size, ?MAX_POOL_SIZE),
+                    MaxOverflow = application:get_env(eredis_cluster, pool_max_overflow, ?MAX_POOL_OVERFLOW_SIZE);
+                _ ->
+                    Size = ets:lookup(?MODULE, pool_size),
+                    MaxOverflow = ets:lookup(?MODULE, pool_max_overflow)
+            end,
 
             PoolArgs = [{name, {local, PoolName}},
                         {worker_module, eredis_cluster_pool_worker},
@@ -57,11 +63,20 @@ stop(PoolName) ->
 get_name(Host, Port) ->
     list_to_atom(Host ++ "#" ++ integer_to_list(Port)).
 
+-spec start_link(Args ::array) -> {ok, pid()}.
+start_link(Args) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
+
 -spec start_link() -> {ok, pid()}.
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
--spec init([])
+-spec init(Args :: array)
 	-> {ok, {{supervisor:strategy(), 1, 5}, [supervisor:child_spec()]}}.
+
+init({PoolSize ,OverFlowSize}) ->
+    ets:new(?MODULE, [protected, set, named_table, {read_concurrency, true}]),
+    ets:insert(?MODULE, [{pool_size, PoolSize}, {pool_max_overflow, OverFlowSize}]),
+    {ok, {{one_for_one, 1, 5}, []}};
 init([]) ->
-	{ok, {{one_for_one, 1, 5}, []}}.
+    {ok, {{one_for_one, 1, 5}, []}}.
--- src/eredis_cluster_sup.erl
+++ src/eredis_cluster_sup.erl
@@ -13,10 +13,19 @@ start_link() ->
 	-> {ok, {{supervisor:strategy(), 1, 5}, [supervisor:child_spec()]}}.
 init([]) ->
     Procs = [{eredis_cluster_pool,
-                {eredis_cluster_pool, start_link, []},
-                permanent, 5000, supervisor, [dynamic]},
-            {eredis_cluster_monitor,
-                {eredis_cluster_monitor, start_link, []},
-                permanent, 5000, worker, [dynamic]}
-            ],
+        {eredis_cluster_pool, start_link, [{100,0}]},
+        permanent, 5000, supervisor, [dynamic]},
+%%            {eredis_cluster_monitor_a,
+%%                {eredis_cluster_monitor, start_link, [Args]},
+%%                permanent, 5000, worker, [dynamic]},
+%%            {eredis_cluster_monitor_b,
+%%                {eredis_cluster_monitor, start_link, [{eredis_cluster_monitor_b, [{"127.0.0.1",10000},{"127.0.0.1",10001},{"127.0.0.1",10002},{"127.0.0.1",10003},{"127.0.0.1",10004},{"127.0.0.1",10005}]}]},
+%%                permanent, 5000, worker, [dynamic]},
+        {eredis_cluster_client_c,
+            {eredis_cluster_client, start_link, [{eredis_cluster_client_c, [{"127.0.0.1",9000},{"127.0.0.1",9001},{"127.0.0.1",9002},{"127.0.0.1",9003},{"127.0.0.1",9004},{"127.0.0.1",9005}]}]},
+            permanent, 5000, worker, [dynamic]},
+        {eredis_cluster_monitor_d,
+            {eredis_cluster_client, start_link, [{eredis_cluster_client_d, [{"127.0.0.1",9000},{"127.0.0.1",9001},{"127.0.0.1",9002},{"127.0.0.1",9003},{"127.0.0.1",9004},{"127.0.0.1",9005}]}]},
+            permanent, 5000, worker, [dynamic]}
+    ],
     {ok, {{one_for_one, 1, 5}, Procs}}.
